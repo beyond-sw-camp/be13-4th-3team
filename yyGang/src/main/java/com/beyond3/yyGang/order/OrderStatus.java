95b46f6e73ed2b80013558e69d323eb5d3246b3b
namoo36 <wldnrkwhr36@naver.com>
2025-04-13 10:52:54 +0900

    20250413

--- yyGang/src/main/java/com/beyond3/yyGang/order/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.beyond3.yyGang.order;
-
-public enum OrderStatus {
-    PENDING, ORDERED, PAID, CANCELLED, IN_TRANSIT, DELIVERED;
-}
--- yyGang/src/main/java/com/beyond3/yyGang/pay/PayStatus.java
+++
@@ -1,5 +0,0 @@
-package com.beyond3.yyGang.pay;
-
-public enum PayStatus {
-    WAITING, FAIL, CANCELLED, SUCCESS
-}
